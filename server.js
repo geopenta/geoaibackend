ed8314fae1d547d8a33d0547cdeeab94058f6525
geopenta <98428208+geopenta@users.noreply.github.com>
2024-12-18 12:47:04 +0200

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,6 @@ app.post('/api/onboard', async (req, res) => {
 
 
 // Start the server
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
